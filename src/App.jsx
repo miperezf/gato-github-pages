3d4c981fd4d3ef790103fe427892c2236a00b9f9
Miguel Perez <miguuuel.perez@gmail.com>
2024-02-29 17:52:58 -0300

    tictactoe

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import confetti from "canvas-confetti";
+import { Square } from "./components/Square.jsx";
+import { TURNS} from "./constants.js";
+import { checkWinner } from "./logic/board.js";
+import { WinnerModal } from "./components/WinnerModal.jsx";
+import { checkEndGame } from "./logic/board.js";
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js";
+
+
+export function HolaMundo() {
+  const [board, setBoard] = useState(() =>{
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
+  })
+    
+  const [turn, setTurn] = useState(() =>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  });
+
+
+
+  const [winner, setWinner] = useState(null);
+
+ 
+
+  const resetGame = () =>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    resetGameStorage()
+  }
+
+
+  const updateBoard = index => {
+    if (board[index] || winner) return;
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+ 
+
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+   saveGameToStorage(
+    {board: newBoard,
+    turn: newTurn
+    }
+
+    )
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      confetti()
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)){
+      setWinner(false)
+    }
+  };
+
+  useEffect(() =>{
+   
+  },[winner])
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
+      <section className="game">
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn == TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn == TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+       <WinnerModal resetGame={resetGame} winner={winner}/>
+    </main>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {HolaMundo} from './App.jsx'
+import './index.css'
+import './App.css'
+
+const root= ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <>
+  <HolaMundo/>
+  </>
+)
+
+
